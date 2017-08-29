02dd00a27059c4c24f5030a906eeb7f04b08f83a
Shardul-Tripathi <shardul.511@gmail.com>
2017-08-29 10:13:47 +0530

    parallelized, initial topics done, found viral?

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
-g++ -std=c++11 model.cpp -o model
-time ./model
+#!/bin/bash
+if [ $1 = 0 ]; then
+	g++ -std=c++11 -fopenmp -O3 -march=native model.cpp -o model
+else	
+	g++ -std=c++11 -fopenmp -march=native model.cpp -o model
+fi
+time ./model $1
