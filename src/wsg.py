3e2b3a4933bab8e0438de8a755dffbad26b6f419
dhairya0901 <dhairya@Dhairyas-MacBook-Pro.local>
2017-10-30 15:40:52 +0530

    Corrections in wsg.py

--- src/wsg.py
+++ src/wsg.py
@@ -3,10 +3,11 @@ import sys
 
 numNodes = int(sys.argv[1])
 degree = int(sys.argv[2])
-rewiring = int(sys.argv[3])
+rewiring = float(sys.argv[3])
 
-g = connected_watts_strogatz_graph(n = numNodes, k = degree, p = rewiring, tries = 100, seed = None)
+g = nx.connected_watts_strogatz_graph(n = numNodes, k = degree, p = rewiring, tries = 100, seed = None)
 e = nx.edges(g)
+print(nx.diameter(g))
 f1 = open('./edges','w+')
 f1.write(str(e))
 f1.close()
