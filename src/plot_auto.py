6805ea7bbc814329edc45cd78cdebaecf894330f
dhairya0901 <dhairya@Dhairyas-MacBook-Pro.local>
2017-11-20 22:53:08 +0530

    Plot for 1096 points over entire range

--- src/plot_auto.py
+++ src/plot_auto.py
@@ -19,20 +19,21 @@ with open('results.txt') as file2:
 		line = line.strip().split()
 		regime[int(line[0])*8 + int(line[1])] = int(line[2])	
 
-supv = [points[i] for i in range(1096) if regime[i] == 1]
-vir = [points[i] for i in range(1096) if regime[i] == 2]
-subv = [points[i] for i in range(1096) if regime[i] == 3]	
+supv = [points[i] for i in range(1096) if regime[i] == 1 and points[i][0] < 4000]
+vir = [points[i] for i in range(1096) if regime[i] == 2 and points[i][0] < 4000]
+subv = [points[i] for i in range(1096) if regime[i] == 3 and points[i][0] < 4000]	
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-for c, m, l in [('y', 'o', supv), ('b', '^', vir), ('r', 'o', subv)]:
+for c, m, l, la in [('y', 'o', supv, 'Super-viral'), ('b', '^', vir, 'Viral'), ('r', 'o', subv, 'Sub-viral')]:
 	points = [[0.0]*1096 for i in range(3)]
 	for i in range(3):
 		points[i] = [row[i] for row in l]
-	ax.scatter(points[0], points[1], points[2], c=c, marker=m)
+	ax.scatter(points[0], points[1], points[2], c=c, marker=m, label=la)
 
 ax.set_xlabel('A')
 ax.set_ylabel('alpha')
 ax.set_zlabel('beta')
-
+plt.legend(loc='upper right')
+plt.savefig('../figures/3dplot1.png')
 plt.show()
--- src/temp.cpp
+++ src/temp.cpp
@@ -10,13 +10,14 @@ int main() {
 	for (A = 10; A < 11000; A += 1000) {
 		for (a = 0.005; a < 0.5; a += 0.05) {
 			for (b = 0.005; b < 0.5; b += 0.05, count = (count+1)%8) {
- 				if (count == 0)
- 					fout << "{";
- 				fout << "{" << A << ", " << a << ", " << b;
- 				if (count == 7)
- 					fout << "}}, ";
- 				else
- 					fout << "}, ";
+ 				// if (count == 0)
+ 				// 	fout << "{";
+ 				// fout << "{" << A << ", " << a << ", " << b;
+ 				// if (count == 7)
+ 				// 	fout << "}}, ";
+ 				// else
+ 				// 	fout << "}, ";
+ 				fout << A << " " << a << " " << b << endl;
  			}
 		}
 	}
