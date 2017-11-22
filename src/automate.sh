72d1e8c1dbba5c2f87b7730b59c4e1932834e4e6
Shardul-Tripathi <shardul.511@gmail.com>
2017-11-22 11:02:40 +0530

    Results for 2200 points in the limited range (A=10-1010)

--- src/automate.sh
+++ src/automate.sh
@@ -3,9 +3,9 @@ n=15000
 k=75
 p=0.2
 g++ -std=c++11 -fopenmp -O2 -march=native model_auto.cpp -o model_auto
-for ((i=0;i<137;i++))
+for ((i=0;i<275;i++))
 do
 	./model_auto 1 $n $i
 	python3 classify_auto.py 8 $i
 done
-python3 plot_auto.py
+#python3 plot_auto.py
